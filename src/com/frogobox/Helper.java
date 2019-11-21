849f35d1b23c03e66a476c688f97befe1ae202ac
amirisback <faisalamircs@gmail.com>
2019-11-21 14:31:59 +0700

    add: Constant, Helper

---
+++ src/com/frogobox/Helper.java
@@ -0,0 +1,20 @@
+package com.frogobox;
+
+/**
+ * Created by Faisal Amir
+ * FrogoBox Inc License
+ * =========================================
+ * TeoriBahasaAutomata
+ * Copyright (C) 21/11/2019.
+ * All rights reserved
+ * -----------------------------------------
+ * Name     : Muhammad Faisal Amir
+ * E-mail   : faisalamircs@gmail.com
+ * Github   : github.com/amirisback
+ * LinkedIn : linkedin.com/in/faisalamircs
+ * -----------------------------------------
+ * FrogoBox Software Industries
+ * com.frogobox
+ */
+public class Helper {
+}
