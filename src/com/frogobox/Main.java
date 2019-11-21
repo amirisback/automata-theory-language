fad802862515a4d4e8c8219713563a5c39f6688a
amirisback <faisalamircs@gmail.com>
2019-11-21 17:23:15 +0700

    add: Tampilan

--- src/com/frogobox/Main.java
+++ src/com/frogobox/Main.java
@@ -1,8 +1,23 @@
 package com.frogobox;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    new Main().programView();
+    }
+
+
+
+    private void mainView(){
+        System.out.println();
+    }
+
+    private void programView(){
+        System.out.print("Input Program : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        System.out.println(input);
     }
 }
