4778f0d171d183958a98c77434727d028edbc397
amirisback <faisalamircs@gmail.com>
2019-11-21 16:43:05 +0700

    add: Tampilan

--- src/com/frogobox/Main.java
+++ src/com/frogobox/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        new Main().mainView();
+
     }
+
+
+    private void mainView(){
+        System.out.println("TUBES IFX 43-02");
+        System.out.println("List Anggota Kelompok");
+        System.out.println("-----------------------------------");
+        System.out.println("Revi Chandra Riana, 1301198512");
+        System.out.println("Hildan Fawaz Naufal, 1301198514");
+        System.out.println("Monica Liviandra, 1301198517 ");
+        System.out.println("Muhammad Faisal Amir, 13011985497");
+        System.out.println("-----------------------------------");
+    }
+
 }
