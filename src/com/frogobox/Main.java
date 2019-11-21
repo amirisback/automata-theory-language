0bc08e5c294967f766ee69e41d0a4b6177bd4734
amirisback <faisalamircs@gmail.com>
2019-11-21 23:50:20 +0700

    update: Main java

--- src/com/frogobox/Main.java
+++ src/com/frogobox/Main.java
@@ -4,6 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
         // Write your code here
+        new Main().introView();
+        new Main().onCreate();
+    }
+
+    private void onCreate(){
+        // Call all function here
+        
+    }
+
+    private void introView(){
+        System.out.println("Muhammad Faisal Amir");
+        System.out.println("Hildan Fawwaz");
+        System.out.println("Monica liviandra");
+        System.out.println("Revi Chandra Riana");
+        System.out.println("IFX - 43 -02");
     }
 
 }
